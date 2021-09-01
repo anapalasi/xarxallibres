df8e443e7948512c3743307ae67d274b3afa661c
anapalasi <anapalasi@gmail.com>
2021-09-01 17:39:40 +0200

    Distribucio per ordre alfabètic

--- distribucioTutoria.php
+++ distribucioTutoria.php
@@ -14,7 +14,7 @@
 	$sentencia= "select id_tutoria, descripcion, id_aula from Tutoria where id_tutoria=\"". $_POST['tutoria']. "\"";
 	$tutoria=executaSentencia($conexion,$sentencia);
 
-    $sentencia="select A.nombre, A.apellido1, A.apellido2, A.id_lote, A.id_lote from Alumno A where A.id_tutoria=\"". $_POST['tutoria']."\" and A.banc_llibres=\"1\" ";
+    $sentencia="select A.nombre, A.apellido1, A.apellido2, A.id_lote, A.id_lote from Alumno A where A.id_tutoria=\"". $_POST['tutoria']."\" and A.banc_llibres=\"1\" order by A.apellido1, A.apellido2, A.nombre";
 	/*$sentencia="select A.nombre, A.apellido1, A.apellido2, A.id_lote, T.id_aula, L.retirat from Alumno A, Historico H, Tutoria T, Lote L where A.id_tutoria=\"". $_POST['tutoria']."\" and A.id_lote = H.id_lote and H.curso=\"2020\" and H.id_tutoria=T.id_tutoria and L.id_lote=A.id_lote order by A.apellido1, A.apellido2, A.nombre";*/
 	$alumnos = executaSentenciaTotsResultats($conexion, $sentencia);
 

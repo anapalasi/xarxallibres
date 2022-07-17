202317b1cb794b6974c41ab17ee2d577ca3f2ded
anapalasi <anapalasi@gmail.com>
2022-07-17 17:55:07 +0200

    Esborrar llibres antics finalitzat

--- esborrantllibres.php
+++ esborrantllibres.php
@@ -17,16 +17,18 @@
 	// Obtenim els exemplars d'eixe llibre
 	$sentencia="select id_ejemplar from Ejemplar where isbn_libro=\"". $isbn . "\"";
 	$llibres = executaSentenciaTotsResultats($conexion,$sentencia);
-	
 
+	echo $sentencia. "<br>";
 	foreach ($llibres as $llibre) {
 		// Esborrem totes les observacions de l'exemplar.
-		$sentencia ="delete from ObservacionEjemplar where id_ejemplar=\"". $llibre['id_ejemplar'] . "\"";
-		executaSentencia($conexion,$setencia);
+		$sentencia ="delete from ObservacionEjemplar where id_ejemplar=\"". $llibre['id_ejemplar'] . "\";";
+		$borradas= $conexion->exec($sentencia);
+		// La sentencia exec nos dice el número de filas afectadas por una sentencia DELETE
+		//executaSentenciaTotsResultats($conexion,$setencia);
 
 
 		// Quan s'ha esborrat totes les observacions, s'esborra el llibre
-    	$sentencia="delete from Ejemplar where id_ejemplar=\"". $llibre['id_ejemplar']."\"";
+    	$sentencia="delete from Ejemplar where id_ejemplar=\"". $llibre['id_ejemplar']."\";";
     	executaSentencia($conexion,$sentencia);
 		
 		$numero=$numero+1;
@@ -34,6 +36,10 @@
 
 	$sentencia="select titulo, id_asignatura from Libro where isbn=\"". $isbn ."\"";
 	$resultat = executaSentencia($conexion,$sentencia);
+
+	// Esborrem el .llibre
+	$sentencia = "delete from Libro where isbn=\"". $isbn ."\";";
+	executaSentencia($conexion, $sentencia);
 
 	require 'views/esborrantllibres.view.php';
 

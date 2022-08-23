0eed9dd7759f04a870b2dd98d8de662c31370ed2
anapalasi <anapalasi@gmail.com>
2022-08-23 18:39:22 +0200

    Alumnat nou carregat

--- actualitzaAlumnat.php
+++ actualitzaAlumnat.php
@@ -35,16 +35,29 @@
 				// Calculamos el curso
 				$curso=calculaCurso()+1;
 				$grupoActual = $curso. "_". $grupo;
-				echo $grupoActual. "<br>";
 				$sentencia="update Alumno set id_tutoria=\"$grupoActual\" where nia=\"$nia\"";
 				// Actualitzem el grup de l'alumne
 				executaSentencia($conexion,$sentencia);
 				
 				
 			}
+			else{ // Alumnat nou
+	
+				$nombre=$datos[1];
+				$apellido1= $datos[2];
+				$apellido2=$datos[3];
+				$grupo=$datos[8];
+				// Calculamos el curso
+				$curso=calculaCurso()+1;
+				$grupoActual = $curso. "_". $grupo;
+				$sentencia="insert into Alumno (nia, nombre, apellido1, apellido2, banc_llibres, id_tutoria, repetidor) values (\"";
+				$sentencia = $sentencia . $nia. "\",\"". utf8_encode($nombre). "\",\"". utf8_encode($apellido1). "\",\"". utf8_encode($apellido2). "\",1,\"". $grupoActual. "\",0)";
+				executaSentencia($conexion,$sentencia);
+			}	
+
 
 		}
-		//else Falta l'alumnat nou
+		
 	}
 	//Cerramos el archivo
 	fclose($archivo);

1193740e9ddab02bfa036861cf70d66476929262
Anabel Palasi <anapalasi@gmail.com>
2023-03-14 13:04:40 +0100

    Creados libros alumnos

--- assignarPuntuacio.php
+++ assignarPuntuacio.php
@@ -19,48 +19,97 @@
 		$tipus=	$_POST["tipus"];
 		if (strcmp(substr($tipus,0,4),"1bat")==0){
 			$subtipus=substr($tipus,-2,2);
+			$curso=calculaCurso();
 			if (strcmp($subtipus,"ac") == 0)
 			{
-				echo "Ciències";
+				$sentencia="select * from Alumno where id_tutoria like '". $curso. "_1BA%' and opcion=\"AC\"";
+				$alumnes=executaSentenciaTotsResultats($conexion,$sentencia);
+				$contador=count($alumnes);
 			}
 			else {
-				echo "Comunes";
-			}		
+			
+				$sentencia="select * from Alumno where id_tutoria like '". $curso. "_1BA%' and opcion is NULL";
+				$alumnes=executaSentenciaTotsResultats($conexion,$sentencia);
+				$contador=count($alumnes);
+			} 
+
 		}
-		else
-			echo "No es Batxillerat";
+/*		else
+	echo "No es Batxillerat";*/
 	}
 	else {
 		// Creació d'un sol lot
 		$contador=1;
 	}	
 	// Crear el lote
-	/*
-	$sentencia="insert into Lote (id_lote, puntos, repartit, folres, valoracioglobal, retirat) VALUES (\"";
-	$sentencia = $sentencia . $identificador;
-	$sentencia = $sentencia . "\", 0,0,1,\"\",0)";
+
+	$i=1;
+	$raiz=substr($identificador,0,7);
 
 	$fecha=date('Y-m-d');
-	// Insertamos lote en la base de datos
-	executaSentencia($conexion, $sentencia);
+
+	if ($contador != 1){
+		echo "Creacion en masa ". $identificador. "<br>";
+		$num=1;
+		foreach ($alumnes as $alumne){
+			// Calculamos el identificador
+			$identificador = $raiz.$i;
+
+			// Creamos el lote
+
+			$sentencia="insert into Lote (id_lote, puntos, repartit, folres, valoracioglobal, retirat) VALUES (\"";
+			$sentencia = $sentencia . $identificador;
+			$sentencia = $sentencia . "\", 0,0,1,\"\",0)";
+	//		echo $sentencia. "<br>";
+
+			// Creamos los libros asociados a un lote
+			
+			$i=0;
+			foreach ($_POST['identificador'] as $identificador){
+				$pos=strripos($identificador,"_");
+				$raiz_libro=substr($identificador,0,$pos+1);
+			      	$ejemplar_libro = $raiz_libro. $num; 
+				echo $ejemplar_libro.	 "<br>";
+						
+				$sentencia ="insert into Ejemplar  (id_ejemplar, puntos, fecha_mod, isbn_libro,volumen_libro,id_lote) values (\"";
+				$sentencia= $sentencia. $ejemplar_libro. "\", ";
+				$sentencia = $sentencia."\"3\"".",\"". $fecha . "\",\"" . $_POST['isbn'][$i]."\",\"". $_POST['volumen'][$i]. "\",\"". strtoupper($_POST['lot']). "\")";
+				echo $sentencia. "<br>";	
+				// executaSentencia($conexion, $sentencia);
+				$i++;
+			}
+			$num++; 	//Actualizamos el número
+			// Asignamos el lote al alumno
+			$nia=$alumne['nia'];
+			echo $nia. " ". $identificador. "<br>";
+			$i++;
+		}
+	}
+	else {
+
+		$sentencia="insert into Lote (id_lote, puntos, repartit, folres, valoracioglobal, retirat) VALUES (\"";
+		$sentencia = $sentencia . $identificador;
+		$sentencia = $sentencia . "\", 0,0,1,\"\",0)";
+		executaSentencia($conexion, $sentencia);
 
 	
-	$i=0;
-	foreach ($_POST['identificador'] as $identificador)
-	{
-		echo $estat[$i]. "<br>";
-		$sentencia ="insert into Ejemplar  (id_ejemplar, puntos, fecha_mod, isbn_libro,volumen_libro,id_lote) values (\"";
-		$sentencia= $sentencia. $identificador. "\", ";
-		$estat = $_POST['estat'][$i];
-		$sentencia = $sentencia.$estat.",\"". $fecha . "\",\"" . $_POST['isbn'][$i]."\",\"". $_POST['volumen'][$i]. "\",\"". strtoupper($_POST['lot']). "\")";
-		executaSentencia($conexion, $sentencia);
-		$i++;
-		//echo $sentencia;
-	}
+		$i=0;
+		foreach ($_POST['identificador'] as $identificador)
+		{
+			echo $estat[$i]. "<br>";
+			$sentencia ="insert into Ejemplar  (id_ejemplar, puntos, fecha_mod, isbn_libro,volumen_libro,id_lote) values (\"";
+			$sentencia= $sentencia. $identificador. "\", ";
+			$estat = $_POST['estat'][$i];
+			$sentencia = $sentencia.$estat.",\"". $fecha . "\",\"" . $_POST['isbn'][$i]."\",\"". $_POST['volumen'][$i]. "\",\"". strtoupper($_POST['lot']). "\")";
+			executaSentencia($conexion, $sentencia);
+			$i++;
+			//echo $sentencia;
+		}
+	
 
 
-	echo "Lot " . strtoupper($_POST["lot"]).  " ha sigut donat d'alta <br>";
-	 */
+		echo "Lot " . strtoupper($_POST["lot"]).  " ha sigut donat d'alta <br>";
+	} 		
 	echo "<center> <a href=\"";
 
   	if ($usuario['rol'] == 'administrador')

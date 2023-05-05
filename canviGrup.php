6ac2218ae42bd217d7476e6d922bdda132f8be9e
Anabel Palasi <anapalasi@gmail.com>
2023-05-05 11:42:39 +0200

    Añadiendo tutorias Batx

--- canviGrup.php
+++ canviGrup.php
@@ -10,7 +10,7 @@ if (!isset($_SESSION['usuario'])) {
 
 $conexion = conexion($bd_config);
 $curso=calculaCurso();
-$sentencia="SELECT id_tutoria, descripcion from Tutoria where id_tutoria like '" . $curso. "_%ESO%' or id_tutoria like '". $curso. "_%CFB%'";
+$sentencia="SELECT id_tutoria, descripcion from Tutoria where id_tutoria like '" . $curso. "_%ESO%' or id_tutoria like '". $curso. "_%CFB%' or id_tutoria like '". $curso . "_%BA%'";
 
 // Obtenemos las tutorias que tienen libros
 $tutorias = executaSentenciaTotsResultats($conexion, $sentencia);
